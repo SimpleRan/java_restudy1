7956ee394fc0cdfae4ce6b99bac56e4cc312c248
shimpleRan <aksent1@naver.com>
2022-05-01 23:34:48 +0900

    day1

--- java project day1/src/day1/Sample10.java
+++ java project day1/src/day1/Sample10.java
@@ -3,8 +3,82 @@ package day1;
 public class Sample10 {
 
 	public static void main(String[] args) {
-		// 포매팅
-
+		// 문자열 포매팅
+		// 문자열에서 또 하나 알아야 할 것으로 문자열 포메팅(Formatting)이 있다
+		// 예를 들어 "현재 온드는 18도 입니다" 
+		// 시간이 지나서 20도가 되면 다음 문장을 출력한다
+		// "현재 온도는 20도 입니다"
+		// 위 두 문자열은 모두 같은데 20이라는 숫자와 18이라는 숫자만 다르다. 이렇게 문자열 안의
+		// 특정한 값을 바꿔야 할 경우가 있을 때 이것을 가능하게 해주는 것이 바로 포매팅기법이다.
+		// 쉽게 말해 문자열 포매팅이란 문자열 안에 어떤 값을 삽입하는 방법이다. 
+		
+		// 1. 숫자 바로 대입
+		System.out.println(String.format("I eat %d apples", 3));
+		// 포매팅은 Sting.format 메소드를 사용하며 위 예제에서 보듯 문자열 안에 정수 3을 삽입하는 방법
+		// 문자열 안에서 숫자를 넣고 싶은 자리에 %d 문자를 넣어주고 , 삽입할 숫자는 3은 두번째 파라미터로 전달
+		// 여기에서 %d는 문자열 포맷 코드라고 부른다.
+		
+		// 2. 문자열에 바로 대입
+		System.out.println(String.format("I eat %s apples.", "five"));
+		// 위 예제에서는 문자열 안에 또 다른 문자열을 삽입하기 이해 앞에서 사용한 문자열포맷코드 %d가 아닌
+		// %s를 썻다 . 숫자를 넣기위해서는 %d를 써야하고 문자열을 넣기위해서는 %s를 써야한다.
+		
+		//3. 숫자값을 나타내는 변수로 대입
+		//int number = 3;
+		//System.out.println(String.format("I eat %d apples.", number));
+		// 1번처럼 숫자를 바로 대입하나 위ㅔ 예제처럼 숫자값을 나타내는 변수를 대입하나 결과는 같다.
+		
+		// 4. 2개 이상의 값 넣기
+		int number = 10;
+		String  day = "three";
+		System.out.println(String.format("I eat %d apples. so I was sick for %s days", number, day));
+		// 위와 같이 2개 이상의 값을 넣으려면 파라미터로 여려개를 전달하면 된다. 
+		
+		// 문자열 포맷 코드 -문자열 포매팅 예제에서는 대입해 넣는 자료형으로 
+		// 정수와 문자열을 사용했으나 이 외에도 다양한 것을 대입할 수 있다
+		
+		// %s	문자열(String)
+		// %c	문자 1개(character)
+		// %d	정수(Integer)
+		// %f	부동소수(floating-point)
+		// %o	8진수
+		// %x	16진수
+		// %%	Literal % (문자 % 자체)
+		
+		// 여기에 재밌는 것은 %s 포맷 코드인데, 이 코드는 어떤 형태의 값이든 변환해 넣을 수 있다. 
+		
+		System.out.println(String.format("I have %s apples",  3));  // "I have 3 apples" 출력
+		System.out.println(String.format("rate is %s", 3.234));  // "rate is 3.234" 출력
+		
+		// [포매팅 연산자 %d와 %를 같이 쓸 때는 %%를 쓴다]
+		System.out.println(String.format("Error is %d%%.", 98));
+		
+		// 포맷 코드와 숫자 함께 사용하기
+		// 1. 정렬과 공백
+		System.out.println(String.format("%10s", "hi"));
+		System.out.println(String.format("%5s", "hi"));
+		// 앞의 예문에서 %10s는 전체 길이가 10개인 문자열 공간에서 대입되는 값을 오른쪽으로 정렬하고
+		// 그 앞의 나머지는 공백으로 남겨두라는 의미이다
+		// 반대쪽인 왼쪽 정렬은 %-10s가 될 것이다. 
+		System.out.println(String.format("%-10sjane", "hi"));
+		// hi를 왼쪽을 정렬하고 나머지는 공백으로 채웠다
+		
+		// 2.소수점 표현하기		
+		System.out.println(String.format("%.4f", 3.42134234)); 
+		// 3.42134234를 소수점 네 번째 까지만 나타낼 수 있다. '.'의 의미는 소수점 포인트를 말하고
+		// 그 뒤 숫자 4는 소수점 뒤에 올 숫자의 개수를 말한다.
+		System.out.println(String.format("%10.4f", 3.42134234));
+		
+		// System.out.printf
+		// String.format 메소드는 포매팅된 문자열을 리턴한다. 따라서 포메팅된 문자열을 출력하려면
+		// 다음처럼 system.out.println 메소드를 함게 사용해야한다
+		System.out.println(String.format("I eat %d apples.", 3));
+		// System.out.printf 메소드를 사용하면 String.format 메소드 없이도 
+		// 동일한 형식으로 포매팅된 문자열을 출력할 수 있다
+		System.out.printf("I eat %d apples.", 3);
+		//String.format 과 System.out.printf의 차이는 전자는 문자열을 리턴하는 메소드이고
+		// 후자는 문자열을 출력하는 메소드라는 점이다. 상황에 맞게 선택하여 사용하자.
+		
 	}
 
 }
--- java project day1/src/day1/Sample9.java
+++ java project day1/src/day1/Sample9.java
@@ -5,25 +5,71 @@ public class Sample9 {
 	public static void main(String[] args) {
 		// 내장 문자열	
 		// String 자료형의 내장 메소드 중에서 자주 사용되는 것들에 대해서 알아보자
-		// equals는 두개의 문자열이 동일한지를 비교하여 결과값을 리턴
 		
-		String a = "hello";
-		String b = "java";
-		String c = "hello";
-		System.out.println(a.equals(b)); // false 출력
-		System.out.println(a.equals(c)); // true 출력
+		// equals - 두개의 문자열이 동일한지를 비교하여 결과값을 리턴
+		
+		// String a = "hello";
+	//	String b = "java";
+	//	String c = "hello";
+	//	System.out.println(a.equals(b)); // false 출력
+	//	System.out.println(a.equals(c)); // true 출력
 		
 		// 문자열 a 와 문자열 b 는 "hello"와 "java"로 서로 같지 않다. 
 		// 따라서 equals 메소드 호출 시 false 를 리턴한다. 
 		// 문자열 a 와 문자열 c 는 "hello"와 "hello"로 서로 같다. 따라서 true 를 리턴함
 		// 문자열의 값을 비교할때는 반드시 equals 를 사용해야 한다. == 연산자를 사용할 경우 다음과 같은 경우가 발생할 수 있다.
-		String a = "hello";
-		String b = new String("hello");
-		System.out.println(a.equals(b));  // true
-		System.out.println(a == b);  // false
+		
+		// String a = "hello";
+		// String b = new String("hello");
+		
+	//	System.out.println(a.equals(b));  // true
+  //	System.out.println(a == b);  // false
 		
 		// 문자열 a와 b는 모두 "hello"로 같은 값이지만 equals 를 호출했을 때는 true 를 
 		// == 연산자를 이용했을 때는 false를 리턴한다. a와 b는 값은 같지만 서로 다른 객체이다. 
 		//== 은 두개의 자료형이 동일한 객체인지를 판별할 때 사용하는 연산자이기 때문에 false를 리턴한다
+		
+		// indexOf - 문자열에서 특정 문자가 시작되는 위치(인덱스)를 리턴한다
+		// String a = "Hello Java";
+		// 위와 같은 a 문자열에서 "Java"라는 문자열이 시작되는 위치를 알고 싶은 경우 indexOf를 다음과 같이 사용할 수 있다
+		//String a = "Hello Java";
+		//System.out.println(a.indexOf("Java"));  // 6 출력
+		
+		// "Hello Java" 라는 문자열에서 "Java"라는 문자열은 일곱번째 문자('J')부터 시작이다. 
+		// 결과값이 6으로 나온 이유는 자바는 숫자를 0부터 세기 때문이다
+		
+		// contains - 문자열에서 특정 문자열이 포함되어 있는지읭 ㅕ부를 리턴한다.
+		
+		//String a = "Hello Java";
+		//System.out.println(a.contains("Java"));  // true 출력
+		// 문자열 a는 "Java"라는 문자열을 포함하고 있기 때문에 true를 리턴
+		
+		// charAt - 문자열에서 득정 문자의 위치(char)를 리턴한다
+		// String a = "Hello Java"; 
+		// 위와 같은 a 문자열에서 "J"라는 문자는 6번째 인덱스에 위치한 문자이다. 
+		// 6이라는 숫자로 "J"라는 문자를 얻기위해서는 다음과 같이 charAt을 사용
+		
+		//System.out.println(a.charAt(6)); // J 출력
+		// J라는 문자는 일곱번째 문자이지만 6으로 찾는 이유는 자바는 숫자를 0부터 세기 때문이다.
+		
+		// replaceAll - 문자열 중 특정 문자열을 다른 문자열로 바꾸고자할대 사용
+		//System.out.println(a.replace("Java", "World")); // Hello World출력
+		
+		// substring - 특정 부분을 뽑아낼 경우 사용
+		//System.out.println(a.substring(0, 4)); //Hell 출력
+		// 위처럼 substring(시작위치, 끝위치)와 같이 사용하면 문자열의 시작위치에서 끝위치까지 문자를
+		// 뽑아내게 된다 단 끝위치는 포함이 안된다는 점에 주의하자 시작위치 <= a < 끝위치
+		
+		// toUpperCase -모두 대문자로 변경할 때 사용한다 (모두 소문자로 변경할 때는 toLowerCase를 사용)
+		//System.out.println(a.toUpperCase());
+	//	System.out.println(a.toLowerCase());
+		
+		// split - 문자열을 특정 구분자로 분리하는 메소드
+		String a = "a:b:c:d";
+		String[] result = a.split(":"); 
+		
+		
+		
+		
 	}
 }
